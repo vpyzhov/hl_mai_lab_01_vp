8974173cf199fc700cd2e68182ffae36c72f8452
VPyzhov <pyzhov.vasily@gmail.com>
2023-11-02 20:15:40 +0300

    -a

--- web_server/http_request_factory.h
+++ web_server/http_request_factory.h
@@ -55,7 +55,7 @@ public:
 
         std::cout << "request:" << request.getURI()<< std::endl;
         if (hasSubstr(request.getURI(),"/user") ||
-            hasSubstr(request.getURI(),"/search") ||
+            hasSubstr(request.getURI(),"/user_search") ||
             hasSubstr(request.getURI(),"/auth")) 
             return new UserHandler(_format);
         return 0;
