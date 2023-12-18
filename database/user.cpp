aec868ae4861be445998fb7899c38eb485a17d91
VPyzhov <pyzhov.vasily@gmail.com>
2023-12-18 23:42:43 +0300

    -a

--- database/user.cpp
+++ database/user.cpp
@@ -243,7 +243,7 @@ namespace database
             Poco::Data::Session session = database::Database::get().create_session();
             Poco::Data::Statement insert(session);
             long id = 1;
-            long sharding = 1;
+            //long sharding = 1;
             //std::string select_str = "INSERT INTO User (id, first_name,last_name,email,role,login,password) VALUES(?, ?, ?, ?, ?, ?, ?)";
             //std::string sharding_hint = " -- sharding:1";
             //select_str += sharding_hint;
