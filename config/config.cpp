d3efdcb5adc17d22d1e9e31e6881b52308d4db2d
VPyzhov <pyzhov.vasily@gmail.com>
2023-12-19 23:53:27 +0300

    -a

--- config/config.cpp
+++ config/config.cpp
@@ -7,7 +7,6 @@ Config::Config()
         _login = std::getenv("DB_LOGIN");
         _password = std::getenv("DB_PASSWORD");
         _database = std::getenv("DB_DATABASE");
-        _cache_servers = std::getenv("CACHE");
 }
 
 Config &Config::get()
@@ -16,15 +15,6 @@ Config &Config::get()
     return _instance;
 }
 
-const std::string &Config::get_cache_servers() const
-{
-    return _cache_servers;
-}
-
-std::string &Config::cache_servers()
-{
-    return _cache_servers;
-}
 
 const std::string &Config::get_port() const
 {
